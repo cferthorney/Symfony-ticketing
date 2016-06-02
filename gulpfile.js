85788858367ecc74ab6d3fe4612785e64dd985fc
David Thorne <david@cferthorney.com>
2016-06-02 19:29:43 +0100

    Gulp reconfiguration
    Sort gulp CSS out

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,39 @@ var sassPaths = [
   'bower_components/motion-ui/src'
 ];
 
+var config = {
+  resourcesDir: 'app/Resources',
+  sassDir: 'scss/**/*.scss',
+  scriptDir: 'js/**',
+  production: !!$.util.env.production,
+  sourcemaps: !$.util.env.production
+};
+
+var app = {};
+
+app.addStyle = function (paths, outputFilename) {
+  return gulp.src(paths)
+      .pipe($.plumber())
+      .pipe($.if(config.sourcemaps, $.sourcemaps.init()))
+      .pipe($.sass({
+        includePaths: sassPaths
+      })
+          .on('error', $.sass.logError))
+      .pipe($.concat(outputFilename))
+      .pipe($.if(config.production, $.cleanCss()))
+      .pipe($.autoprefixer({
+        browsers: ['last 2 versions', 'ie >= 9']
+      }))
+      .pipe($.if(config.sourcemaps, $.sourcemaps.write('.')))
+      .pipe(gulp.dest('web/css'));
+}
+
 gulp.task('sass', function() {
-  return gulp.src('app/Resources/scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('app/Resources/public/css'));
+  app.addStyle([
+    config.resourcesDir + '/' + config.sassDir
+    ], 'main.css');
 });
 
 gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch([config.resourcesDir + '/' + config.sassDir], ['sass']);
 });
